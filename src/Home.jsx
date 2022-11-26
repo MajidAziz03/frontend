25811989d6654bae2434e6c8c6ade84489816778
Majid Aziz <azizmajid34@gmail.com>
2022-11-26 12:19:56 +0500

    Completed Navbar

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import "./Home.scss";
+
+const Home = () => {
+    return (
+        <>
+            <div className="home">
+                <Navbar />
+            </div>
+        </>
+    );
+};
+
+export default Home;
